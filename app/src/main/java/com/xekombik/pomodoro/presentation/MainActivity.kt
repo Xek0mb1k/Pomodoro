5052303e5b96d3385fb60e516bcfeeeae1b198fd
Xek0mb1k <oganes969@gmail.com>
2023-03-28 23:29:02 +0300

    Initial commit

---
+++ app/src/main/java/com/xekombik/pomodoro/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xekombik.pomodoro.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xekombik.pomodoro.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
